c9e5fbf0efe9492b81e6706c527600d3c122bc6a
Ramu <ramu2115@gmail.com>
2026-09-24 09:17:52 +0530

    sumOfThreeNumbers is added

--- src/main/java/com/github/Practise.java
+++ src/main/java/com/github/Practise.java
@@ -7,11 +7,17 @@ public class Practise {
         System.out.println("Welcome to Github Actions practise");
         Practise practise = new Practise();
         int sumOfTwoNumbers = practise.sumOfTwoNumbers(10, 10);
+        System.out.println("sumOfTwoNumbers......"+sumOfTwoNumbers);
     }
 
     public int sumOfTwoNumbers(int number1, int number2)
     {
         return number1 + number2;
+    }
+
+    public int sumOfThreeNumbers(int number1, int number2, int number3)
+    {
+        return number1 + number2 + number3;
     }
 
 }
